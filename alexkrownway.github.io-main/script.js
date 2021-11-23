f3846fa04986e7e5778ad53589bf3061775cca6e
Alex Krownway <61385083+AlexKrownway@users.noreply.github.com>
2021-11-23 18:08:11 +0300

    123
    123

---
+++ alexkrownway.github.io-main/script.js
@@ -0,0 +1,196 @@
+document.body.innerHTML += `
+  <div style="height: 16px"></div>
+`
+
+  document.body.innerHTML += `
+
+
+
+
+  <div class="box"><div>
+    
+      
+  <div style="height: 100%; width: 100%; display: flex;">
+  <div style="display: inline-block;">
+  <iframe src="https://open.spotify.com/embed/album/5DKYcwByM2GO4XNZArBluE?utm_source=generator&theme=0" width="400" height="450" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  </div>
+
+  <div style="display: flex; flex-direction: column; width: 100%; margin-left: 32px; margin-right: 32px;">
+    <div style="height: 100%; text-align: center;">
+      <h1>Music Collection</h1>
+        <h3>A few Commissions/personal works from early June this year !</h3>
+        <h3></h3>
+ 
+    </div>
+
+    <div>
+      <ul>
+        <li style="display: inline;"><a href="https://open.spotify.com/album/5DKYcwByM2GO4XNZArBluE?si=IfH4FefhTPW4EyU7aJPCVQ"><i class="fab fa-spotify"> </i></a></li>
+        <li style="display: inline;"><a href="https://alexkrownway.bandcamp.com/album/music-collection"><i class="fab fa-bandcamp"></i></a></li>
+        <li style="display: inline;"><a href="https://music.apple.com/us/album/music-collection-ep/1583774590"><i class="fab fa-itunes"></i></a></li>
+        <li style="display: inline;"><a href="https://www.youtube.com/watch?v=5cAV86SSBP0"><i class="fab fa-youtube"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+  </div></div>
+  <div class="box"><div>
+    
+      
+  <div style="height: 100%; width: 100%; display: flex;">
+  <div style="display: inline-block;">
+  <iframe src="https://open.spotify.com/embed/album/5ru0HWM4FGQJYCweSUK5HN?utm_source=generator&theme=0" width="400" height="450" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  </div>
+
+  <div style="display: flex; flex-direction: column; width: 100%; margin-left: 32px; margin-right: 32px;">
+    <div style="height: 100%; text-align: center;">
+      <h1>The Banished Light</h1>
+        <h3>The album commissioned by @Haziq_Heartfelt for his Chapter 1 of</h3>
+        <h3>well, The Banished Light ;)</h3>
+ 
+    </div>
+
+    <div>
+      <ul>
+        <li style="display: inline;"><a href="https://open.spotify.com/album/5ru0HWM4FGQJYCweSUK5HN?si=SKiCcawPRcGXkJE7qGlRAw"><i class="fab fa-spotify"> </i></a></li>
+        <li style="display: inline;"><a href="https://alexkrownway.bandcamp.com/album/the-banished-light-original-soundtrack"><i class="fab fa-bandcamp"></i></a></li>
+        <li style="display: inline;"><a href="https://music.apple.com/us/album/the-banished-light/1583004659"><i class="fab fa-itunes"></i></a></li>
+        <li style="display: inline;"><a href="https://www.youtube.com/watch?v=-0iTk7ReSOE"><i class="fab fa-youtube"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+  </div></div>
+
+
+
+
+    <div class="box"><div>
+    
+      
+    <div style="height: 100%; width: 100%; display: flex;">
+    <div style="display: inline-block;">
+    <iframe src="https://open.spotify.com/embed/album/7rCNxdub0WyrWr3d2k202w" width="400" height="450" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </div>
+
+    <div style="display: flex; flex-direction: column; width: 100%; margin-left: 32px; margin-right: 32px;">
+      <div style="height: 100%; text-align: center;">
+        <h1>The Music of Oricord</h1>
+          <h3>Truly an Amazing project I had worked on and my first major paybacks to the amazing Ori Discord Community</h3>
+   
+      </div>
+
+      <div>
+        <ul>
+          <li style="display: inline;"><a href="https://open.spotify.com/album/7rCNxdub0WyrWr3d2k202w?si=82mrbrMkRxy-3P8hsQqqKQ"><i class="fab fa-spotify"> </i></a></li>
+          <li style="display: inline;"><a href="https://alexkrownway.bandcamp.com/album/the-music-of-oricord"><i class="fab fa-bandcamp"></i></a></li>
+          <li style="display: inline;"><a href="https://music.apple.com/us/album/the-music-of-oricord/1565972171"><i class="fab fa-itunes"></i></a></li>
+          <li style="display: inline;"><a href="https://www.youtube.com/watch?v=c7jWTtGSgs4&"><i class="fab fa-youtube"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+    </div></div>
+
+
+
+
+
+
+
+
+    <div class="box"><div>
+    
+  <div style="height: 100%; width: 100%; display: flex;">
+  <div style="display: inline-block;">
+  <iframe src="https://open.spotify.com/embed/album/1UP1E8lsAvvT2rNm63vyUH" width="400" height="450" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  </div>
+
+  <div style="display: flex; flex-direction: column; width: 100%; margin-left: 32px; margin-right: 32px;">
+    <div style="height: 100%; text-align: center;">
+      <h1>Asymmetry</h1>
+      <h3>The OST for a game that never will see the light of the day</h3>
+    </div>
+
+    <div>
+      <ul>
+      <li style="display: inline;"><a href="https://open.spotify.com/album/1UP1E8lsAvvT2rNm63vyUH?si=dn24oWB-Rtqk7yXf1KvY3Q"><i class="fab fa-spotify"> </i></a></li>
+      <li style="display: inline;"><a href="https://alexkrownway.bandcamp.com/album/asymmetry"><i class="fab fa-bandcamp"></i></a></li>
+      <li style="display: inline;"><a href="https://music.apple.com/us/album/asymmetry/1557691836"><i class="fab fa-itunes"></i></a></li>
+      <li style="display: inline;"><a href="https://www.youtube.com/watch?v=DQFR-MX8wkU"><i class="fab fa-youtube"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</div></div>
+
+
+
+
+
+
+
+<div class="box"><div>
+    
+<div style="height: 100%; width: 100%; display: flex;">
+<div style="display: inline-block;">
+<iframe src="https://open.spotify.com/embed/album/4kUcQMFnlsxPI5r2FyajEb" width="400" height="450" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+</div>
+
+<div style="display: flex; flex-direction: column; width: 100%; margin-left: 32px; margin-right: 32px;">
+  <div style="height: 100%; text-align: center;">
+    <h1>Divinest Light</h1>
+    <h3>The official album for the game</h3>
+  </div>
+
+  <div>
+    <ul>
+    <li style="display: inline;"><a href="https://open.spotify.com/album/4kUcQMFnlsxPI5r2FyajEb?si=B1NuCqHST3OIJWlkbC1RUQ"><i class="fab fa-spotify"> </i></a></li>
+    <li style="display: inline;"><a href="https://alexkrownway.bandcamp.com/album/divinest-light-original-game-soundtrack-2"><i class="fab fa-bandcamp"></i></a></li>
+    <li style="display: inline;"><a href="https://music.apple.com/us/album/divinest-light-original-game-soundtrack/1543167801"><i class="fab fa-itunes"></i></a></li>
+    <li style="display: inline;"><a href="https://www.youtube.com/watch?v=FiujfWt5C_I&"><i class="fab fa-youtube"></i></a></li>
+    </ul>
+  </div>
+</div>
+</div>
+</div></div>
+      
+  `;
+// }
+
+requestAnimationFrame(() => {
+  const boxes = document.querySelectorAll(".box");
+
+  function getIntersectionRatio(i) {
+    const a = [window.scrollY, window.scrollY + window.innerHeight];
+    const b = [boxes[i].offsetTop, boxes[i].offsetTop + boxes[i].clientHeight];
+
+    const max = Math.max(a[0], b[0]);
+    const min = Math.min(a[1], b[1]);
+
+    return Math.max(0, (min - max) / (b[1] - b[0]));
+  }
+
+  function onScroll() {
+    const boxes = document.querySelectorAll(".box");
+    for (let i = 0; i < boxes.length; i += 1) {
+      const intersection = getIntersectionRatio(i);
+      const top = boxes[i].offsetTop - window.pageYOffset < 0;
+      boxes[i].firstChild.style.cssText = `
+        transform-origin: ${top ? "center center" : "top center"};
+        position: ${top ? "fixed" : "absolute"};
+        transform: scale(${intersection});
+        opacity: ${intersection};
+      `;
+    }
+    requestAnimationFrame(onScroll);
+  }
+
+  onScroll();
+});
+
+// Option with less CPU usage, but also less FPS / smooth
+// window.addEventListener('scroll', onScroll);
